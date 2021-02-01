0dd0e62ca578592eb1e0d1e4c74655f56158abf5
Ahsan Siddique <ahsan.siddique@kinettix.com>
2021-02-01 19:54:07 +0500

    Add project files.

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic.Weather;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using ViewModels.WeatherModels;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherBLL _weatherBLL;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherBLL weatherBLL)
+        {
+            _logger = logger;
+            _weatherBLL = weatherBLL;
+        }
+
+        [HttpGet("{SearchTerm}")]
+        [AllowAnonymous]
+        public IEnumerable<WeatherForecastModel.Period> Get(string SearchTerm)
+        {
+            var forecast = new List<WeatherForecastModel.Period>();
+            var response = _weatherBLL.GetWeatherDetails(SearchTerm);
+            if (response != null)
+            {
+                if (response.properties != null)
+                {
+                    forecast = response.properties.periods;
+                }
+            }
+            return forecast;
+        }
+    }
+}
---
+++ API/Infrastructure/DI/CustomDI.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLogic.GeoCoding;
+using BusinessLogic.Weather;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+using Services.GeoCoding;
+using Services.Weather;
+
+namespace API.Infrastructure.DI
+{
+    public static class CustomDI
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IWeatherService, WeatherService>();
+            services.AddScoped<IWeatherBLL, WeatherBLL>();
+
+            services.AddScoped<IGeoCodingService, GeoCodingService>();
+            services.AddScoped<IGeoCodingBLL, GeoCodingBLL>();
+        }
+    }
+}
